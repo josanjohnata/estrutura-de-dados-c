2f594f7839b1572160317e5e891b6e88b8bc699b
John Pontes <jpontes@questrade.com>
2025-07-19 12:25:22 -0300

    Algoritmos e estruturas de dados

---
+++ estruturas-de-dados-heterogeneas/04-estrutura-de-dados-heterogeneas.c
@@ -0,0 +1,36 @@
+// Algoritmos e estruturas de dados
+
+// Estrutura de dados é um ramo da computação que estuda mecanismos para organizar e armazenar dados 
+// de forma eficiente, permitindo que algoritmos os manipulem de maneira otimizada. Algoritmos são 
+// conjuntos de passos usados para realizar tarefas específicas e, quando aplicados a dados organizados 
+// em estruturas adequadas, resultam em programas mais eficientes e eficazes.
+
+
+// Portanto, a escolha correta das estruturas de dados e dos algoritmos é essencial para o desenvolvimento 
+// de software que precisa processar grandes volumes de dados.
+
+// Estrutura de dados
+
+// É o ramo da computação que estuda os diversos mecanismos de organização de dados para atender aos diferentes 
+// requisitos de processamento.
+
+
+// Resumindo
+// A estrutura de dados é um modo de armazenar e organizar os dados no computador, de uma maneira que eles possam 
+// ser usados eficientemente.
+
+
+// As estruturas de dados definem a organização, os métodos de acesso e as opções de processamento para coleções de 
+// itens de informação manipulados pelo programa. As estruturas são formas de distribuir e relacionar os dados 
+// disponíveis, de modo a tornar mais eficientes os algoritmos que manipulam esses dados.
+
+// Mas o que é algoritmo?
+
+// Algoritmo é um conjunto de passos finitos e organizados, que são usados para executar uma tarefa específica. Ele 
+// funciona como um bloco de informações, permitindo que dispositivos como computadores funcionem.
+
+// Já o programa é um código feito de algoritmos que dizem o que deve ser realizado.
+
+// Os algoritmos manipulam os dados. Quando os dados estão dispostos de forma coerente, caracterizam uma forma, uma 
+// estrutura de dados. Geralmente, os algoritmos são elaborados para manipulação de dados e, quando os dados estão 
+// organizados de forma coerente, representam uma estrutura de dados.

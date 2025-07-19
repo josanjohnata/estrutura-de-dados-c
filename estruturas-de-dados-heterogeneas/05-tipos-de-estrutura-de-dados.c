072465c9d24b3c021a2691006c519d3ac3fd7c23
John Pontes <jpontes@questrade.com>
2025-07-19 12:32:57 -0300

    Tipos de estrutura de dados

---
+++ estruturas-de-dados-heterogeneas/05-tipos-de-estrutura-de-dados.c
@@ -0,0 +1,11 @@
+// Tipos de estrutura de dados
+
+// As estruturas de dados são fundamentais para a organização e manipulação eficiente de informações em 
+// um programa de computador. Elas podem ser homogêneas, com elementos do mesmo tipo, como vetores e 
+// matrizes, ou heterogêneas, com elementos de tipos diferentes, como registros. Tipos de dados básicos 
+// incluem inteiro, real, lógico e texto.
+
+
+// Estruturas como listas, filas, pilhas, árvores e grafos são utilizadas para diferentes propósitos, 
+// desde armazenamento sequencial até representação de relações complexas. A escolha adequada da estrutura 
+// de dados é fundamental para a eficiência dos algoritmos que manipulam esses dados.

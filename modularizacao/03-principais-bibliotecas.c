ec7b7567aee0561e96b15d59847743d6d0f5dfcd
John Pontes <jpontes@questrade.com>
2025-07-22 21:30:03 -0300

    Principais bibliotecas

---
+++ modularizacao/03-principais-bibliotecas.c
@@ -0,0 +1,39 @@
+// Principais bibliotecas
+
+// As funções predefinidas na linguagem C são essenciais para facilitar diversas operações comuns, 
+// como manipulação de strings, entrada e saída de dados, e cálculos matemáticos. Essas funções estão 
+// organizadas em bibliotecas específicas, acessíveis por meio de arquivos header com extensão “.h”. 
+// Para utilizar essas funções, é necessário incluir as bibliotecas apropriadas no início do programa 
+// usando a diretiva #include. Conhecer as bibliotecas padrão, como stdio.h, stdlib.h, ctype.h, time.h, 
+// dos.h, string.h e math.h, permite ao programador aproveitar ao máximo as funcionalidades oferecidas 
+// pela linguagem C.
+
+// Conceitos
+// As funções predefinidas na linguagem C estão estipuladas nas bibliotecas da linguagem, nos arquivos 
+// header com extensão “.h”. Para usá-las nos programas, é necessário incluir no início do mesmo, através 
+// da diretiva #include, a biblioteca que possui a função que será usada.
+
+
+// Dessa maneira, existem diversos header padrão na linguagem C, entre eles:
+
+// stdio.h
+// Funções de E/S padrão nos dispositivos stdin, stdout e files.
+
+// stdlib.h
+// Funções de E/S padrão nos dispositivos stdin, stdout e files. Funções para conversão de números em cadeias 
+// de caracteres.
+
+// ctype.h
+// Funções para tratamento de caractere.
+
+// time.h
+// Funções para manipulação de horários e datas.
+
+// dos.h
+// Funções de acesso as INT's da BIOS e DOS do MS-DOS.
+
+// string.h
+// Funções de manipulação de strings (TC).
+
+// math.h
+// Funções matemáticas em geral.

31bf773603c2e95e554f08cf1ea92db9a09bb8b7
John Pontes <jpontes@questrade.com>
2025-07-27 18:11:23 -0300

    Tipo de lista: Pilha

---
+++ 03-listas-pilhas-e-filas/08-tipo-de-lista-pilha.c
@@ -0,0 +1,9 @@
+// Tipo de lista: Pilha
+// Veremos agora um tipo particular de lista chamada pilha. Uma pilha é um tipo de lista na qual as 
+// operações de inserção, remoção e acesso ocorrem sempre numa mesma extremidade, chamada de topo.
+// Comentário
+// Por essa razão, apenas a posição do topo precisa ser monitorada.
+
+// Uma pilha segue a regra “O último a chegar é o primeiro a sair”, também conhecida pela sigla LIFO, 
+// do inglês Last In, First Out. Colocando de outra maneira, a remoção ocorre na ordem inversa da 
+// inserção. Devido à essa característica, pilhas têm a propriedade de inverter sequências.

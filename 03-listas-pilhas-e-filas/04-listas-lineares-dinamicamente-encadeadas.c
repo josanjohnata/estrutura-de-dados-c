42870ca2825124f1950e684f84a4b5f3f5d92a0a
John Pontes <jpontes@questrade.com>
2025-07-27 16:05:53 -0300

    Listas lineares dinamicamente encadeadas

---
+++ 03-listas-pilhas-e-filas/04-listas-lineares-dinamicamente-encadeadas.c
@@ -0,0 +1,11 @@
+// Listas lineares dinamicamente encadeadas
+// Para compreendermos as vantagens de se utilizar a alocação encadeada, convém primeiramente 
+// olharmos mais detidamente para a manipulação de memória em um computador.
+
+// Ao longo do tempo, devido à execução de múltiplos programas, alocações e desalocações de 
+// memória vão deixando espaços com tamanhos distintos disponíveis. Esse problema é chamado de 
+// fragmentação de memória e vai tornando cada vez mais difícil alocar posições contíguas de 
+// memória na heap.
+
+// Comentário
+// O impacto mais claro da fragmentação de memória é no desempenho.

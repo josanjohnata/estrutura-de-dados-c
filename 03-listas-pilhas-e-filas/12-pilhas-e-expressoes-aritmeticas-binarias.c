1bed76f603aee955f84950cd5099a35a46d90498
John Pontes <jpontes@questrade.com>
2025-07-27 21:58:11 -0300

    Pilhas e expressões aritméticas binárias

---
+++ 03-listas-pilhas-e-filas/12-pilhas-e-expressoes-aritmeticas-binarias.c
@@ -0,0 +1,7 @@
+// Pilhas e expressões aritméticas binárias
+// A manipulação de expressões aritméticas é um tema importante, que requer uma forma adequada de se 
+// representar computacionalmente tais expressões.
+
+// Usualmente, expressões aritméticas possuem operadores, que representam as operações aritméticas, 
+// operandos, sobre os quais os operadores atuam, e delimitadores, utilizados para estabelecer a 
+// precedência das operações.

b494e904c5099b53b22e9f56bf53809d55ec847b
John Pontes <jpontes@questrade.com>
2025-08-01 09:32:11 -0300

    Árvores

---
+++ 05-arvores/01-arvores.c
@@ -0,0 +1,16 @@
+// Árvores
+// Em computação, a forma como os dados são armazenados tem impacto para operações de inserção, 
+// remoção, atualização, seleção de mínimo/máximo, entre outras. Por isso, estruturas que tornem 
+// tais operações eficientes são de grande interesse para os profissionais da área. As árvores 
+// oferecem vantagens para a realização dessas operações, como veremos nas próximas seções.
+
+// Atenção
+// Estudaremos nesta seção os principais conceitos envolvendo árvores, as propriedades básicas e a 
+// terminologia que você precisa dominar para entender o assunto. Nosso objetivo será desenvolver 
+// gradualmente o entendimento da estrutura de dado, permitindo a conexão entre os conceitos e 
+// definições mostrados. Com isso, estabeleceremos as bases para discussões mais elaboradas que 
+// serão conduzidas nos demais módulos.
+
+// Esta seção oferecerá o ferramental para as discussões mais aprofundadas das próximas. Para 
+// facilitar o seu estudo, os conceitos, propriedades e terminologias apresentam-se destacados 
+// em negrito, facilitando a sua localização no texto.

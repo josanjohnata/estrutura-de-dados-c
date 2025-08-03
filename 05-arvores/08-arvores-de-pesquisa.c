87191e969441966a48555b04e93dc7359297bd18
John Pontes <jpontes@questrade.com>
2025-08-03 14:07:54 -0300

    Árvores de pesquisa

---
+++ 05-arvores/08-arvores-de-pesquisa.c
@@ -0,0 +1,18 @@
+// Árvores de pesquisa
+// Esta seção coroa o estudo realizado. Ela se propõe a apresentar um uso prático de árvores. 
+// Você verá como árvores são utilizadas para solucionar problemas de busca.
+
+// Este tipo de problema surge quando dado um conjunto de elementos, cada um identificado por 
+// uma chave (a qual é única), se quer localizar um elemento específico mediante a procura por 
+// sua chave.
+
+// Aplicar árvores a este tipo de problema significa distribuir os elementos e suas respectivas 
+// chaves pelos nós da árvore. Para fins didáticos, em nossos exemplos e discussões, vamos considerar 
+// apenas a chave, que é o indexador do conjunto. Porém, uma vez que há vínculo entre a chave e o 
+// elemento do conjunto, recuperar a chave é recuperar o elemento.
+
+// O problema de busca está presente em diversas aplicações. Já mencionamos antes o uso de árvores 
+// em bancos de dados, mas elas também são empregadas, por exemplo, na indexação de arquivos, em 
+// sistemas operacionais, de espaços usados e disponíveis de alocação em disco e várias outras 
+// situações. Por esta razão, encontrar maneiras de se otimizar as operações em árvores tem grande 
+// relevância. Por isso, examinaremos, também, o balanceamento de árvores.
